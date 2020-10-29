d87e630fd3d661585e34a7ab1bd890228df2194b
cgonzales <cgonzales@turner-industries.com>
2020-10-29 14:14:25 -0500

    feat: added routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import FnTodo from './functional/FnTodo';
 import ClassTodo from './class/ClassTodo';
 
 const App = () => {
   React.useEffect(() => document.title = 'React TODO', []);
 
   return (
-    <div>
-      <ClassTodo />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/class">
+          <ClassTodo />
+        </Route>
+        <Route path="/function">
+          <FnTodo />
+        </Route>
+        <Route path="/">
+          <div>
+            Go to /class for the class based component, and /function for the functional component.
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
